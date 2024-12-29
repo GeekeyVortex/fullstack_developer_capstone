3a33ecb9ef65a2d38473f7471eaf31eaf7d01e45
GeekryVortex <nikaljeprajwal@gmail.com>
2024-12-29 22:45:20 +0530

    hopefully

---
+++ xrwvm-fullstack_developer_capstone/server/djangoapp/urls.py
@@ -0,0 +1,21 @@
+# Uncomment the imports before you add the code
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+from django.views.generic import TemplateView
+
+
+app_name = 'djangoapp'
+urlpatterns = [
+    # # path for registration
+
+    # path for login
+    # path(route='login', view=views.login_user, name='login'),
+    # path('about/', TemplateView.as_view(template_name="About.html")),
+
+    # path for dealer reviews view
+
+    # path for add a review view
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
